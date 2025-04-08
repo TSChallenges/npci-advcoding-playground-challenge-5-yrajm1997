4cba1cb502ff5a24bba2ccd67b6693060e616c45
Yograj Mahawar <89453936+yrajm1997@users.noreply.github.com>
2025-04-08 15:04:24 +0530

    Update Flight.java

--- src/main/java/com/example/api/model/Flight.java
+++ src/main/java/com/example/api/model/Flight.java
@@ -1,78 +1,56 @@
-package com.example.api.model;
+package com.hackerrank.api.model;
 
-/*import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-
-@Entity
-public class Flight {
-    @Id
-    private Long id;
-}
-
- */
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import java.util.*;
 
 @Entity
-@Table(name="flights")
 public class Flight {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
-    @Column(name = "flight")
     private String flight;
-
-    @Column(name = "origin")
     private String origin;
-
-    @Column(name = "destination")
     private String destination;
+    private List<Integer> speed_series;
 
-    @Column(name = "speed_series")     //@JsonProperty("speed_series")
-    private Integer[] speedSeries;
-
-    public String getDestination() {
-        return destination;
-    }
-
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
-
-    public Integer[] getSpeedSeries() {
-        return speedSeries;
-    }
-
-    public void setSpeedSeries(Integer[] speedSeries) {
-        this.speedSeries = speedSeries;
-    }
-
-    public String getOrigin() {
-        return origin;
-    }
-
-    public void setOrigin(String origin) {
-        this.origin = origin;
-    }
-
-    public String getFlight() {
+public Integer getId(){
+    return id;
+}
+    public String getFlight(){
         return flight;
     }
 
-    public void setFlight(String flight) {
-        this.flight = flight;
+    public String getOrigin(){
+        return origin;
     }
 
-    public Integer getId() {
-        return id;
+    public String getDestination(){
+        return destination;
+    }
+
+    public List<Integer> getSpeedSeries(){
+        return speed_series;
+    }
+
+    public void setId(Integer id){
+        this.id=id;
+    }
+    public void setFlight(String flight){
+        this.flight=flight;
+    }
+
+    public void setOrigin(String origin){
+        this.origin=origin;
+    }
+
+    public void setDestination(String destination){
+        this.destination=destination;
+    }
+
+    public void setSpeedSeries(List<Integer> speedSeries){
+        speedSeries=speed_series;
     }
 }
