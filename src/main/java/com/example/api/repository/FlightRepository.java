28cf4da0389828d1d976102963f917d26f941bc6
Yograj Mahawar <89453936+yrajm1997@users.noreply.github.com>
2025-04-08 15:04:50 +0530

    Update FlightRepository.java

--- src/main/java/com/example/api/repository/FlightRepository.java
+++ src/main/java/com/example/api/repository/FlightRepository.java
@@ -1,10 +1,7 @@
-package com.example.api.repository;
+package com.hackerrank.api.repository;
 
-import java.util.List;
-
-import com.example.api.model.Flight;
+import com.hackerrank.api.model.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FlightRepository extends JpaRepository<Flight, Integer> {
-    List<Flight> findByOrigin(String origin);
 }
